5695ade35d2b479577362d195fdc28ee6519d980
= <help.studentgrade@gmail.com>
2022-05-04 13:49:20 +0600

    added Touchable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
  
@@ -7,13 +7,17 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1}>Hello React</Text>
       <StatusBar style="auto" />
+      
+      <TouchableOpacity>
       <Image
         source={{
           width: 200,
           height: 300,
           uri : "https://picsum.photos/200/300"}}
       />
+      </TouchableOpacity>
     </SafeAreaView>
+
   );
 }
 
