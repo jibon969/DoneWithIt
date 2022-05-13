774b754e8bfd4a3824bfe5937ccab4b4968078a9
= <help.studentgrade@gmail.com>
2022-05-13 23:33:39 +0600

    added flexDirection

--- app/screens/FlexBoxDirection.js
+++ app/screens/FlexBoxDirection.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const FlexBoxScreen = () => {
+const FlexBoxDirection = () => {
     return (
         <View style={{
-            backgroundColor:"#FFF",
-            flex:1,
-            flexDirection:"column-reverse"
+            backgroundColor:"red",
+            flex:1
         }}>
-            <View style={{
+            <View  style={{
                 backgroundColor:"blue",
-                height:100,
-                width:100
+                flex:1
                 }}
             >
             </View>
             <View  style={{
                 backgroundColor:"red",
-                height:100,
-                width:100
+                flex:1
                 }}>
             </View>
 
             <View  style={{
                 backgroundColor:"orange",
-                height:100,
-                width:100
+                flex:1
                 }}>
             </View>
         </View>
     );
 };
 
-export default FlexBoxScreen;
+export default FlexBoxDirection;
